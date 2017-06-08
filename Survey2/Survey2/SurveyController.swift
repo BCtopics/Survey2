78e0de7f0e0124d7dce94ae72d429375d082c4e5
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-06-08 10:55:09 -0600

    Implement PUT Request Functionality

--- Survey2/Survey2/SurveyController.swift
+++ Survey2/Survey2/SurveyController.swift
@@ -14,4 +14,36 @@ class SurveyController {
     
     static var surveys: [Survey] = []
     
+    //MARK: - Get Request
+    
+
+    //MARK: - Put Request
+    
+    static func putSurveyWith(name: String, response: String, identifier: String, completion: @escaping (Bool) -> Void) {
+        
+        let survey = Survey(username: name, response: response, identifier: identifier)
+        
+        guard let endPoint = baseURL?.appendingPathComponent(survey.identifier).appendingPathExtension("json") else { completion(false); return }
+
+        NetworkController.performRequest(for: endPoint, httpMethod: .put, urlParameters: nil, body: survey.jsonData) { (data, error) in
+            
+            if let error = error { NSLog("Error performing network request for PUT. \(error.localizedDescription)"); completion(false); return }
+            
+            guard let data = data, let responseDataString = String(data: data, encoding: .utf8) else {
+                completion(false); NSLog("Data is nil"); return
+            }
+            
+            if responseDataString.contains("error") {
+                NSLog("Error: \(responseDataString)")
+                completion(false)
+                return
+            } else {
+                NSLog("Successfully saved survey to endpoint \(responseDataString)")
+                completion(true)
+            }
+        }
+    }
+    
+// End
 }
+// End
