e182ca252deae4634222837891072a1b0b869e52
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-06-08 10:35:23 -0600

    Implement NetworkController

---
+++ Survey2/Survey2/SurveyController.swift
@@ -0,0 +1,17 @@
+//
+//  SurveyController.swift
+//  Survey2
+//
+//  Created by Bradley GIlmore on 6/8/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import Foundation
+
+class SurveyController {
+    
+    static let baseURL = URL(string: "https://survey-95ee2.firebaseio.com/")
+    
+    static var surveys: [Survey] = []
+    
+}
